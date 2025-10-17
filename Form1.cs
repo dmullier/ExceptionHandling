e1ba9ddc6f2fed88792e0a9cf8e1334a3ea51fdc
Duncan Mullier <D.Mullier@leedsbeckett.ac.uk>
2025-10-17 16:56:47 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,19 @@
+namespace ExceptionHandling
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void divide()
+        {
+            int num1, num2, result;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            divide();
+        }
+    }
+}
